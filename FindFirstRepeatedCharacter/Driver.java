4e80a9184ee454de94182a123e38aec4f9b91282
Sunjay Dhama <sdsunjay73@yahoo.com>
2018-04-01 20:31:00 -0700

    Intial commit from March 2016

---
+++ FindFirstRepeatedCharacter/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+    public static void main(String[] args){
+        String s = "appla"; 
+        System.out.println("Original String: " + s);
+            char a = FindFirstRepeatedCharacter.findFirstRepeatedCharacter(s);
+       System.out.println("First repeated characters is: "+ a);
+    }
+    
+}
