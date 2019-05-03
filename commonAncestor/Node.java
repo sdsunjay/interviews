551b21dfaa68a274159a154b898ebc66b97b2374
Sunjay <sdsunjay73@yahoo.com>
2019-05-02 21:59:09 -0600

    Implemented lowest common ancestor

--- commonAncestor/Node.java
+++ commonAncestor/Node.java
@@ -4,10 +4,11 @@ class Node {
 	Node left;
 	Node right;
 	char A;
+  int val;
 
-
-	public Node(char A, Node parent, Node left, Node right) {
+	public Node(char A, Node parent, Node left, Node right, int val) {
 		this.A = A;
+    this.val = val;
 		this.parent = parent;
 		this.left = left;
 		this.right = right;
@@ -39,7 +40,7 @@ class Node {
 		}
 		else
 			System.out.print("  ");
-			
+
 		if(right!=null)
 		{
 			System.out.print(right);
@@ -47,9 +48,18 @@ class Node {
 		}
 		else
 			System.out.print("  ");
-		
-	} 
-	
+
+	}
+  public Node commonAncestor(Node root, Node p, Node q){
+    if(p.val < root.val && q.val < root.val){
+      return commonAncestor(root.left, p, q);
+    }
+    else if(p.val > root.val && q.val > root.val){
+      return commonAncestor(root.right, p, q);
+    }
+    return root;
+  }
+
 
 }
 
